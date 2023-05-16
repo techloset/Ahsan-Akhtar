6cb64fc8d949c9573351c0e259cc0d647003ca9f
AhsanAkhtar02 <ahsanntucs@gmail.com>
2023-05-16 16:28:55 +0500

    create flask structure

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
-
+#mongodb+srv://ahsan:1095@eventapp.6ancirw.mongodb.net/
 
 if __name__=="__main__":
     app.run()
